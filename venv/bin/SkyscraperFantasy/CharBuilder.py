373c022e2eb36e9d5615637c2a77c9e765608fc7
Evie Diver <e.phazon@gmail.com>
2018-12-16 17:18:35 -0600

    Added skill JSON file.

--- venv/bin/SkyscraperFantasy/CharBuilder.py
+++ venv/bin/SkyscraperFantasy/CharBuilder.py
@@ -1,11 +1,12 @@
 import json
 import logging
+from enum import IntEnum
 
 
 class CharBuilder:
 
     def __init__(self):
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO, format='%(msg)s')
         logging.info('Character Builder Initializing...')
         versionJSON = json.load(open("data/default/version.json"))
         self.version = (versionJSON['major'], versionJSON['minor'], versionJSON['patch'], versionJSON['tag'])
@@ -18,6 +19,21 @@ class CharBuilder:
         for race in raceJSON:
             self.races.append(Race(race))
         logging.info(f"Loaded {len(self.races)} races.")
+
+        logging.info('Loading Skills...')
+        self.skills = list()
+        skillJSON = json.load(open('data/default/skills.json'))
+        for skill in skillJSON:
+            self.skills.append(Skill(skill))
+        logging.info(f'Loaded {len(self.skills)} skills.')
+
+        self.skills.sort(key= lambda x: (x.type, x.name))
+        currType = ""
+        for skill in self.skills:
+            if currType != skill.type:
+                currType = skill.type
+                logging.info(f"{currType} skills:")
+            logging.info(f'\t{skill}')
 
 
 class Race:
@@ -36,7 +52,18 @@ class Race:
 
 
 class Skill:
-    pass
+
+    def __init__(self, j):
+        self.name = j['name']
+        self.type = j['type']
+        self.description = j['description']
+        self.default_rank = Rank[str.upper(j['default'])]
+
+    def __str__(self):
+        return f"{self.name} ({self.default_rank.name})"
+
+    def __repr__(self):
+        return f"{self.name} ({self.default_rank.name})"
 
 
 class Ability:
@@ -45,3 +72,12 @@ class Ability:
 
 class Trait:
     pass
+
+class Rank(IntEnum):
+    A = 5
+    B = 4
+    C = 3
+    D = 2
+    E = 1
+    U = 0
+    X = -1
